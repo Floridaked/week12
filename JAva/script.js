8da59e256229bcc50a60255df5de12e034b74a7e
Mariel <Mariel@yahoo.com>
2022-11-23 14:23:33 -0800

    aya finish

--- JAva/script.js
+++ JAva/script.js
@@ -16,4 +16,62 @@ function changeBGpink()
        document.body.classList.add("pink-bg");
 }
 
-clickMeButton.addEventListener("click", changeBGpink)
+clickMeButton.addEventListener("click", changeBGpink);
+
+function changtext()
+{
+    if(clickMeButton.textContent === 'Click Me!' )
+    {
+        clickMeButton.textContent = 'Clicked!';
+    }
+    else if (clickMeButton.textContent === 'Clicked!')
+    {
+        clickMeButton.textContent = 'Click Me!';
+    }
+    clickMeButton.textContent
+}
+
+clickMeButton.addEventListener("click", changtext);
+
+
+
+function updateimage()
+{
+    console.log(Event.
+    const image = document.querySelector('#shoppingCart');
+    //console.log(image);
+    image.src = "images/263142.png";
+    image.width=100;
+    image.height=100;
+    image.alt = "Shoppingcart";
+}
+clickMeButton.addEventListener('click', updateimage);
+
+const buttonContainer = document.querySelector(".button-container");
+// console.log(buttonContainer);
+
+function changeBGGreen(e)
+{
+    if(e.target.tagname === "BUTTON"){
+    // e.target.style.backgroundColor = 'green';
+    e.target.classlist.add("greenBG")
+}
+
+buttonContainer.addEventListener('mouseover', changeBGGreen);
+
+function changeTextColor(e)
+{
+    console.log(e.target.textContent);
+    e.target.style.color = e.target.textContent;
+}
+buttonContainer.addEventListener('click', changeTextColor);
+
+
+
+
+
+
+
+
+
+
